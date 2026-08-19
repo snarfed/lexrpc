c1480d30d22a24315ea77df3bcda83d7fbca256b
Ryan Barrett <git@ryanb.org>
2026-08-18 17:10:04 -0700

    noop: tweak to log suppression in tests

--- lexrpc/tests/__init__.py
+++ lexrpc/tests/__init__.py
@@ -1,9 +1,17 @@
-import logging, os, sys
+import io, logging, os, sys
 
 # Piggyback on unittest's -v and -q flags to show/hide logging.
 logging.basicConfig()
 if '-v' in sys.argv:
     logging.getLogger().setLevel(logging.DEBUG)
-elif 'discover' in sys.argv or '-q' in sys.argv or '--quiet' in sys.argv:
-    logging.disable(logging.CRITICAL + 1)
+else:
+    # used to be:
+    #   elif 'discover' in sys.argv or '-q' in sys.argv or '--quiet' in sys.argv:
+    # dropped that to suppress logging when running full single test files
 
+    # don't emit logs. do this instead of setLevel() or disable() so that the log
+    # messages still get evaluated and raise the same exceptions that they would if
+    # they got emitted.
+    handler = logging.getLogger().handlers[0]
+    if hasattr(handler, 'setStream'):
+        handler.setStream(io.StringIO())
